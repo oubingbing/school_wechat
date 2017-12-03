1a156b3ac98d49244693705935dd0f8830796aa3
xzf0587 <875307054@qq.com>
2017-12-03 22:12:26 +0800

    完成卖舍友上下刷新加载更多数据

--- pages/sale/sale.js
+++ pages/sale/sale.js
@@ -1,4 +1,4 @@
-
+const util = require("../../utils/util.js");
 const app = getApp();
 let genderArray = ['男', '女', '人妖', '未知生物'];
 
@@ -6,9 +6,30 @@ Page({
   data: {
     sales:[],
     baseImageUrl: app.globalData.imageUrl,
+    currentTime:'',
+    pageSize: 10,
+    pageNumber: 1,
+    initPageNumber: 1,
+    showGeMoreLoadin: false,
+    notDataTips: false
   },
   onLoad: function () {
     this.getList();
+
+    //设置当前时间
+    this.setData({
+      currentTime: util.formatTime(new Date())
+    });
+    console.log('当前时间：' + this.data.currentTime);
+
+
+  },
+  onShow:function(){
+
+    console.log('当前时间：' + this.data.currentTime);
+
+    this.getMostNewData();
+
   },
   /**
    * 进入发表页面
@@ -40,9 +61,18 @@ Page({
 
     let _this = this;
 
-    app.http('get', '/sale_friends', {}, res => {
+    app.http('get', '/sale_friends', {
+      page_size: this.data.pageSize,
+      page_number: this.data.pageNumber
+    }, res => {
+
+      this.setData({
+        showGeMoreLoadin: false
+      });
       
+      console.log('返回的贴子数据');
       console.log(res.data.data.page_data);
+      console.log('第几页' + this.data.pageNumber);
 
       let data = res.data.data.page_data.map((item,index)=>{
 
@@ -52,11 +82,97 @@ Page({
 
       });
 
-      _this.setData({
-        sales:res.data.data.page_data
+      let sales = _this.data.sales;
+
+      if (data.length > 0) {
+        data.map(item => {
+          sales.push(item);
+        });
+
+        this.setData({
+          sales: sales,
+          pageNumber: this.data.pageNumber + 1
+        });
+      } else {
+        this.setData({
+          notDataTips: true
+        });
+      }
+
+    });
+
+  },
+
+  /**
+* 下拉刷新，获取最新的贴子
+*/
+  onPullDownRefresh: function () {
+
+    console.log('当前时间：' + this.data.currentTime);
+
+    this.getMostNewData();
+  },
+
+  /**
+ * 上拉加载跟多
+ */
+  onReachBottom: function () {
+
+    console.log('到底了');
+
+    let _this = this;
+
+    this.setData({
+      notDataTips: false
+    });
+
+    this.setData({
+      showGeMoreLoadin: true
+    });
+
+    this.getList();
+
+  },
+
+  /**
+   * 获取当前最新的贴子
+   */
+  getMostNewData:function(){
+
+    let _this = this;
+
+    let time = this.data.currentTime;
+
+    app.http('get', '/most_new_sale_friend?time='+time, {}, res => {
+
+      let sales = _this.data.sales;
+
+      res.data.data.map(item => {
+
+        let ifRepeat = false;
+        for(let sale of sales){
+          if(sale.id == item.id){
+            ifRepeat = true;
+          }
+        }
+
+        if(!ifRepeat){
+          sales.unshift(item);
+        }
+
       });
 
-      console.log(_this.data.sales);
+      _this.setData({
+        sales:sales
+        });
+
+      wx.stopPullDownRefresh();
+
+      console.log(res);
+
+      _this.setData({
+        currentTime: util.formatTime(new Date())
+      });
 
     });
 
@@ -111,8 +227,24 @@ Page({
 
     let _this = this;
 
-    app.http('post', `/praise`, { obj_id: objId, obj_type: objType }, res => {
+    app.http('post', `/praise`, {
+       obj_id: objId, 
+       obj_type: objType 
+       }, res => {
       console.log('点赞成功' + res);
+
+      let sales = _this.data.sales;
+      let newSales = sales.map(item=>{
+        if(item.id == objId){
+          item.praise_number += 1;
+        }
+
+        return item;
+      });
+
+      _this.setData({
+        sales:newSales
+      });
 
     });
 

22741adbe37c3ce1fe439ef7fef3634cc5071b4d
xzf0587 <875307054@qq.com>
2018-01-15 10:05:11 +0800

    chat

--- pages/letter/letter.js
+++ pages/letter/letter.js
@@ -1,4 +1,6 @@
 const util = require('../../utils/util.js')
+const qiniuUploader = require("../../utils/qiniuUploader");
+const uploader = require("../../utils/uploadImage");
 const app = getApp();
 
 Page({
@@ -8,7 +10,12 @@ Page({
     content:'',
     list:[],
     to:12,
-    scrollTop:1200
+    scrollTop:3500,
+    pageSize: 10,
+    pageNumber: 1,
+    initPageNumber: 1,
+    imageArray: [],
+    baseImageUrl: app.globalData.imageUrl,
   },
   onLoad: function (option) {
     
@@ -53,27 +60,109 @@ Page({
 
   },
 
-/**
- * 获取消息 
- */
-getMessageList:function(id){
+  /**
+   * 获取消息 
+   */
+  getMessageList:function(id,oprateType=null){
 
     let _this = this;
 
-    app.http('get', `/message/${id}/list`,
+    let pageSize = _this.data.pageSize;
+    let pageNumber = _this.data.pageNumber;
+    
+    app.http('get', `/message/${id}/list?page_size=${pageSize}&page_number=${pageNumber}`,
       {},
       function (res) {
 
+        wx.stopPullDownRefresh();
+
         console.log(res.data.data);
-        let data = res.data.data;
+        let data = res.data.data.page_data;
+
+        let list = _this.data.list;
+
+        if (oprateType == 'unshift'){
+          if(data.length == 0){
+            wx.showLoading({
+              title: '没有更多记录了~_~',
+            })
+
+            setTimeout(function () {
+              wx.hideLoading()
+            }, 2000)
+          }
+          data.map(item => {
+            list.unshift(item);
+          })
+        }else{
+          data.map(item => {
+            list.push(item);
+          })
+        }
 
         _this.setData({
-          list:data
+          list: list,
+          pageNumber: _this.data.pageNumber + 1
         })
         
       });
 
   },
+
+  /**
+  * 下拉刷新，获取最新的贴子
+  */
+  onPullDownRefresh: function () {
+
+    let friendId = this.data.friendId;
+
+    this.getMessageList(friendId,'unshift');
+
+  },
+
+  /**
+   * 撤回消息
+   */
+  deleteContent:function(e){
+    console.log('长按事件');
+
+    let objId = e.currentTarget.dataset.objid;
+    let _this = this;
+
+    console.log(objId);
+
+    wx.showModal({
+      title: '提示',
+      content: '确定撤回该消息吗',
+      success: function (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+
+          app.http('delete', `/delete/${objId}/chat_message`,
+            {},
+            function (res) {
+
+              let list = _this.data.list;
+              let newList = list.filter((item, index) => {
+
+                if (item.id != objId) {
+                  return item;
+                }
+
+              });
+
+              _this.setData({
+                list: newList
+              })
+
+            });
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+  },
+
   /** 获取输入内容 */
   getContent: function (event) {
     console.log("评论框输入内容:" + event.detail.value);
@@ -89,15 +178,24 @@ getMessageList:function(id){
   send:function(){
     let friendId = this.data.friendId;
     let content = this.data.content;
+    let attachments = this.data.imageArray;
     let _this = this;
+
+    _this.setData({
+      imageArray: []
+    })
 
     console.log(friendId)
     console.log(content)
 
+    if (content == '' && attachments.length == 0){
+      return;
+    }
+
     app.http('post', `/send/${friendId}/message`,
      {
        content:content,
-       attachments:''
+       attachments: attachments
      }, 
      function (res) {
        _this.setData({
@@ -117,7 +215,7 @@ getMessageList:function(id){
 
       setTimeout(function () {
         wx.pageScrollTo({
-          scrollTop: _this.data.scrollTop += 700
+          scrollTop: _this.data.scrollTop += 1000
         })
       }, 500); 
 
@@ -159,6 +257,66 @@ getMessageList:function(id){
 
         });
 
-    }, 5000);
-  }
+    }, 8000);
+  },
+  /**
+   * 发图片
+   */
+  sendImage:function(){
+
+      console.log('select image');
+      let _this = this;
+
+      wx.chooseImage({
+        count: 9, // 默认9
+        sizeType: ['original', 'compressed'],
+        sourceType: ['album', 'camera'],
+        success: function (res) {
+
+          var filePaths = res.tempFilePaths;
+          let position = res.tempFilePaths.length - 1;
+
+          console.log(res.tempFilePaths);
+
+          wx.showLoading({
+            title: '发送中',
+          })
+
+          filePaths.map((item, index) => {
+
+            uploader.upload(item, key => {
+              console.log('上传图片成功的回调');
+              console.log(position);
+              if (position == index) {
+                wx.hideLoading();
+              }
+
+              if (key != '' || key != null) {
+                //提交到服务器
+                let tempImageArray = _this.data.imageArray;
+                tempImageArray.push(key)
+                _this.setData({
+                  imageArray: tempImageArray
+                })
+                _this.send();
+              }
+            })
+
+          });
+
+        }
+      })
+  },
+  /** 预览图片 */
+  previewImage: function (event) {
+
+    console.log(event.target.id);
+
+    let url = event.target.id;
+
+    wx.previewImage({
+      current: '',
+      urls: [url]
+    })
+  },
 })

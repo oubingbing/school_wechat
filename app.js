ef5111f22f6c4118454d4674f34d5c711c7de594
ouzhibing <ouzhibing@galaxymx.com>
2018-05-23 16:02:00 +0800

    恢复一些代码

--- app.js
+++ app.js
@@ -21,159 +21,74 @@ App({
     }
 
   },
-
-  /** 
-   * 登录
-   */
-  login: function () {
-
-    let _this = this;
-
-    // 登录
+  /**
+     * 登录获取token
+     */
+  login: function (_method = null, _url = null, _data = null, callback = null) {
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
         console.log(res);
-        wx.request({
-          url: this.globalData.apiUrl + '/auth/login?type=weChat', //仅为示例，并非真实的接口地址
-          header: {
-            'content-type': 'application/json' // 默认值
-          },
-          method: 'POST',
-          data: {
-            code: res.code
-          },
-          success: function (res) {
-            console.log(res.data);
-            let openId = res.data.data;
-
-            //获取token
-            _this.getToken(_this, openId);
-          }
-        })
+        this.getUserInfo(res.code, _method, _url, _data, callback);
       }
     })
-
   },
 
-  /** 
-   * 获取token
+  /**
+   * 获取用户信息 
    */
-  getToken: function (_this, openId,callback=null) {
-
-    console.log('function getToen');
-
-    // 获取用户信息
+  getUserInfo: function (code, _method = null, _url = null, _data = null, callback = null) {
+    console.log('get user info');
+    let that = this;
     wx.getSetting({
       success: res => {
+        console.log(res);
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
-              _this.globalData.userInfo = res.userInfo
+              this.globalData.userInfo = res.userInfo
 
-              console.log('用户openid' + openId);
+              console.log("用户信息：" + JSON.stringify(res.userInfo));
 
               wx.request({
-                url: _this.globalData.apiUrl + '/auth/token',
+                url: this.globalData.apiUrl + '/auth/login?type=weChat',
                 header: {
                   'content-type': 'application/json'
                 },
                 method: 'POST',
                 data: {
-                  open_id: openId,
-                  user_info: res.userInfo
+                  user_info: res.userInfo,
+                  code: code
                 },
                 success: function (res) {
-                  console.log(res.data);
                   wx.setStorageSync('token', res.data.data);
-
-                  console.log('获得token');
-
-                  if(callback){
-                    callback(res.data.data);
+                  console.log('token:' + res.data.data);
+                  if (_method) {
+                    that.http(_method, _url, _data, callback);
                   }
                 }
               })
 
             }
           })
+        } else {
+          console.log('未授权');
         }
       }
     })
-
   },
 
   /** 
-   * 刷新本地token
+  * 封装微信http请求
   */
-  refreshToken: function (_this, _app, callback = null) {
-
-    console.log('刷新token');
-
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        console.log(res);
-        wx.request({
-          url: _app.globalData.apiUrl + '/auth/login?type=weChat',
-          header: {
-            'content-type': 'application/json'
-          },
-          method: 'POST',
-          data: {
-            code: res.code
-          },
-          success: function (res) {
-            console.log(res.data);
-            let openId = res.data.data;
-
-            //获取新的token
-            wx.getUserInfo({
-              success: res => {
-                wx.request({
-                  url: _app.globalData.apiUrl + '/auth/token',
-                  header: {
-                    'content-type': 'application/json'
-                  },
-                  method: 'POST',
-                  data: {
-                    open_id: openId,
-                    user_info: res.userInfo
-                  },
-                  success: function (res) {
-                    console.log(res.data);
-                    wx.setStorageSync('token', res.data.data);
-
-                    if (callback) {
-                      callback(res);
-                    }
-
-                  }
-                })
-
-              }
-            })
-
-          }
-        })
-      }
-    })
-
-  },
-
-  /** 
-   * 封装微信http请求
-   */
   http: function (_method, _url, _data, callback) {
+
+    console.log('method：' + _method);
 
     let token = wx.getStorageSync('token');
     let _this = this;
-
-    console.log('url:' + this.globalData.apiUrl + _url);
-
-    console.log('http token:' + token);
 
     wx.request({
       url: this.globalData.apiUrl + _url,
@@ -185,45 +100,15 @@ App({
       data: _data,
       success: function (res) {
 
-        console.log(res.data);
-
-        if (res.data.error_message) {
-
-          if (res.data.error_code == 4000) {
-            console.log('token非法');
-            setTimeout(function(){
-              _this.refreshToken(_this, _this, _this.http(_method, _url, _data, callback));
-            },2000);
-          }
-
-          if (res.data.error_code == 4004) {
-            console.log('未授权');
-            setTimeout(function(){
-              _this.refreshToken(_this, _this, _this.http(_method, _url, _data, callback));
-            },2000);
-          }
-
-          if (res.data.error_code == 4001) {
-            console.log('token过期了');
-            setTimeout(function(){
-              _this.refreshToken(_this, _this, _this.http(_method, _url, _data, callback));
-            },2000);
-          }
-
-          if (res.data.error_code == 5000) {
-            console.log('token缺失');
-            setTimeout(function(){
-              _this.refreshToken(_this, _this, _this.http(_method, _url, _data, callback));
-            },2000);
-          }
-
-        }else{
-          wx.hideLoading();
+        if (res.data.error_code) {
+          console.log('token非法');
+          _this.login(_method, _url, _data, callback);
+        } else {
           callback(res);
         }
-    
+
       },
-      fail:function(res){
+      fail: function (res) {
         console.log(res);
         console.log('出错了');
       }

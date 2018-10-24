7bafbdc618e23d934ac2b38723bdcbe9e6bdaa71
ouzhibing <ouzhibing@galaxymx.com>
2018-10-24 14:55:21 +0800

    调整登录策略

--- utils/http.js
+++ utils/http.js
@@ -1,5 +1,58 @@
 const config = require("./../config.js");
 
+/**
+* 登录获取token
+*/
+const login = function (_method = null, _url = null, _data = null, callback = null) {
+  wx.login({
+    success: res => {
+      // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      console.log(res);
+      getUserInfo(res.code, _method, _url, _data, callback);
+    }
+  })
+}
+
+/**
+* 获取用户信息 
+*/
+const getUserInfo = function (code, _method = null, _url = null, _data = null, callback = null) {
+  console.log('get user info');
+  let that = this;
+  wx.getSetting({
+    success: res => {
+      console.log(res);
+      if (res.authSetting['scope.userInfo']) {
+        // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+        wx.getUserInfo({
+          success: res => {
+            // 可以将 res 发送给后台解码出 unionId
+            post("/auth/login_v2?type=weChat", {
+              encrypted_data: res.encryptedData,
+              code: code,
+              iv: res.iv,
+              app_id: config.alianceKey
+            }, function (res) {
+              wx.setStorageSync('token', res.data.data);
+              console.log('token:' + res.data.data);
+              if (_method) {
+                httpRequest(_method, _url, _data, callback);
+              }
+              if (callback) {
+                //回调函数
+                callback();
+              }
+            });
+
+          }
+        })
+      } else {
+        console.log('未授权');
+      }
+    }
+  })
+}
+
 const get=function ( _url, _data, callback) {
   httpRequest("GET",_url,_data,callback);
 }
@@ -37,7 +90,7 @@ const httpRequest=function (_method, _url, _data, callback) {
 
       if (res.data.error_code == '4001' || res.data.error_code == '4000') {
         console.log('token过期了');
-        _this.login(_method, _url, _data, callback);
+        login(_method, _url, _data, callback);
       } else {
         callback(res);
       }
@@ -49,5 +102,5 @@ const httpRequest=function (_method, _url, _data, callback) {
   })
 }
 
-module.exports = { get, post, patch, put, httpDelete, httpRequest}
+module.exports = { get, post, patch, put, httpDelete, httpRequest, login}
 

5280aa87dd9fb3f1f297099c5dce5bdfc68e6c10
bingbing <875307054@qq.com>
2020-10-19 00:10:22 +0800

    调整轮播图

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const config={
-  alianceKey:"b7qHzobPlKTEECJy",//后台分配的allianceKey
+  alianceKey:"cYbR6TesmcnSl7ow",//后台分配的allianceKey
   dev:{//开发环境
     domain:"http://127.0.0.1:8000/api/wechat",//后台接口地址
     qiniuDomain:"https://image.qiuhuiyi.cn",//七牛地址
     bgImage:"tmp/wx0f587d7c97a68e2b.o6zAJs3oh85Zb1lJE8oWix57vny0.91gGjMXALWNEf6b9dd803a7fe4bf5f75b6afd5705a73.jpg"//个人中心背景图片
   },
   prod:{//生产环境
-    domain: "https://love.qiuhuiyi.cn/api/wechat",
+    domain: "https://love.jiucheng.xyz/api/wechat",
     qiniuDomain: "https://image.qiuhuiyi.cn",
     bgImage: "tmp/wx0f587d7c97a68e2b.o6zAJs3oh85Zb1lJE8oWix57vny0.91gGjMXALWNEf6b9dd803a7fe4bf5f75b6afd5705a73.jpg"
   }
 }
 
-//const domain = config.prod.domain;
-const domain = config.dev.domain;
+const domain = config.prod.domain;
+//const domain = config.dev.domain;
 
 const qiniuDomain = config.prod.qiniuDomain;
 const bgImage = config.prod.qiniuDomain;
